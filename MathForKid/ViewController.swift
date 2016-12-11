df8de3a659ff807b33f184581339a9d6788befec
tuan nguyen <nguyenvantuan@nguyens-mac.local>
2016-12-11 21:38:25 +0700

    Initial Commit

---
+++ MathForKid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MathForKid
+//
+//  Created by Nguyen Van Tuan on 12/11/16.
+//  Copyright © 2016 MyProjects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
